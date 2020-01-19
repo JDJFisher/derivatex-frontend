78f03b9d4d3114cf30f98d59eca96a287c03a6d9
Louis Irwin <coding@louisirwin.co.uk>
2020-01-19 21:10:15 +0000

    👷 Made actions branch specific

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <div id="nav">
       Hello World!
+      I'm on dev!
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
